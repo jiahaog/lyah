c45b7e7d9a0ec468c71f160f179b2d2ab6e1df41
Goh Jia Hao <jiahaog@gmail.com>
2017-08-09 17:44:19 +0800

    Rest of chapter 7

--- src/ch07-modules.hs
+++ src/ch07-modules.hs
@@ -1,6 +1,9 @@
 import Data.List (nub, tails)
 import Data.Char (ord, chr)
-import Data.Map as Map
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Geometry
+import Geometry.Cube as Cube
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -31,3 +34,26 @@ findKey key ((k, v):xs) = if key == k
 
 findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey' key = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing
+
+fromList' :: Ord k => [(k, v)] -> Map.Map k v
+fromList' = foldr (\(k, v) acc -> Map.insert k v acc) Map.empty
+
+phoneBook =
+    [("betty","555-2938")
+    ,("betty","342-2492")
+    ,("bonnie","452-2928")
+    ,("patsy","493-2928")
+    ,("patsy","943-2929")
+    ,("patsy","827-9162")
+    ,("lucille","205-2928")
+    ,("wendy","939-8282")
+    ,("penny","853-2492")
+    ,("penny","555-2111")
+    ]
+
+phoneBookToMap :: Ord k => [(k, a)] -> Map.Map k [a]
+phoneBookToMap = Map.fromListWith (++) . map (\(k, v) -> (k, [v]))
+
+testGeometry = cubeArea 2
+
+testGeoCube = Cube.area 2
